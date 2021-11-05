d9a316b7c99691873e2cc9d36873874897a9901d
Morgan Chang <morganchang@Morgans-MacBook-Pro.local>
2021-11-05 15:01:29 -0400

    restructure files

---
+++ src/routes/time_series.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request
+import csv
+import codecs
+
+time_series = Blueprint('time_series', __name__)
+
+# Test: curl -H "Content-Type: multipart/form-data" -F "file=@mydata.csv" http://localhost:5000/myroute
+@time_series.route("/data", methods=['POST'])
+def load_data():
+    file = request.files['file']
+    # print(data['file'].read())
+    data = []
+    stream = codecs.iterdecode(file.stream, 'utf-8')
+    for row in csv.reader(stream, dialect=csv.excel):
+        if row:
+            data.append(row)
+    print(data[0])
+    
+    file_name = file.filename
+    print(file_name)
+
+    return "success"
+
+@time_series.route("/cases", methods=['POST'])
+def query_data():
+    return
