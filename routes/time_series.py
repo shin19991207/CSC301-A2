0b479f969aaf0f1418751e6fe74d98b5e42b495a
Morgan Chang <morganchang@Morgans-MacBook-Pro.local>
2021-11-05 13:43:43 -0400

    time_series

--- routes/time_series.py
+++ routes/time_series.py
@@ -1,10 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 time_series = Blueprint('time_series', __name__)
 
 @time_series.route("/data", methods=['POST'])
 def load_data():
-    return
+    data = request.get_data()
+    print(data)
+    return "success"
 
 @time_series.route("/cases", methods=['POST'])
 def query_data():
